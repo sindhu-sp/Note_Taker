1b1b5a3af3c5552218ee39c3ad8b1b4a2a18dafc
Sindhu Pillai <sindhupillai@Sindhus-MacBook-Pro.local>
2021-07-17 11:23:42 -0700

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const path = require("path");
 const { json } = require('express');
 
